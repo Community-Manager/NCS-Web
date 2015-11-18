9db5b084ab04b34a6d7bef1ba4e7645310cd6f5b
MichaelaIvanova <michaela.av.ivanova@gmail.com>
2015-11-18 13:20:14 +0200

    Added simple all communities view
    route - /communities

--- Source/UI/NeighboursCommunityManager.UI/app/communities/communitiesController.js
+++ Source/UI/NeighboursCommunityManager.UI/app/communities/communitiesController.js
@@ -17,7 +17,18 @@
         function getCommunities() {
             return datacontext.getAvailableCommunities().then(function (data) {
 
-                vm.communities = data;
+                vm.communities = data.data;
+                console.log("only data" + vm.communities);
+                
+                console.log(vm.communities.length);
+
+                for (var i = 0; i < vm.communities.length; i += 1) {
+                    console.log(vm.communities[i].Name);
+                    console.log(vm.communities[i].Description);
+                }
+
+
+                console.log(vm.communities + "here");
                 return vm.communities;
             });
         }
